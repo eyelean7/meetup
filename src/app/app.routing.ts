4e507060395037a0968801069ebca4e882f80898
dan jacob and zach beecher <dan+zach@>
2017-06-19 13:15:22 -0700

    add materialize

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,14 @@
-
+import { WelcomeComponent } from './welcome/welcome.component'
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NewMeetupComponent } from './new-meetup/new-meetup.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const appRoutes: Routes = [
-  // {
-  //   path: '',
-  //   component: WelcomeComponent
-  // },
+  {
+    path: '',
+    component: WelcomeComponent
+  },
   {
     path: 'new-meetup',
     component: NewMeetupComponent
