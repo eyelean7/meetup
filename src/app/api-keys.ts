7eb9d58068b310ce83c7353a40c3232772f2b553
dan jacob and zach beecher <dan+zach@>
2017-06-19 10:15:00 -0700

    attach Firebase

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyCaFYoscVA7qh5ZBZdNKP6rXJ3AFTkgzZ4",
+  authDomain: "meetup-bf7cf.firebaseapp.com",
+  databaseURL: "https://meetup-bf7cf.firebaseio.com",
+  storageBucket: "meetup-bf7cf.appspot.com",
+  messagingSenderId: "489613352171"
+  };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { NewMeetupComponent } from './new-meetup/new-meetup.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
